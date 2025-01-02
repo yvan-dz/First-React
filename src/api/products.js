0476c492f854a145a10ed4c035e2f388cad20277
yvan-dz <yvandzefak1@gmail.com>
2025-01-02 20:28:15 +0100

    update

--- src/api/products.js
+++ src/api/products.js
@@ -1,18 +1,13 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/products';
-
 export const fetchProducts = async () => {
   try {
     const response = await fetch('https://cdn.jsdelivr.net/gh/yvan-dz/First-React@main/db.json');
     if (!response.ok) {
-      throw new Error('Fehler beim Laden der Produkte');
+      throw new Error(`HTTP-Fehler: ${response.status}`);
     }
     const data = await response.json();
-    return data.products; // Produkte aus dem JSON-Objekt zurückgeben
+    return data.products; // Array mit Produkten zurückgeben
   } catch (error) {
-    console.error('Fehler in fetchProducts:', error);
+    console.error('Fehler beim Abrufen der Produkte:', error.message);
     return [];
   }
 };
-
